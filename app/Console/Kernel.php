a6b4442a361b0943244a6c724c50e1ddd54a4d84
Nikul <Nikul>
2022-08-19 17:25:16 +0530

    added new changes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -19,7 +19,7 @@ class Kernel extends ConsoleKernel
     
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('demo:cron')->everyFiveMinutes();
+        $schedule->command('demo:cron')->everyTwoMinutes();
     }
 
     /**
